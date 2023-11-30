a4dd5286376e3331961886127154250be6144d07
P00min <718780@student.dusd.net>
2023-11-30 15:25:17 -0800

    Finished 3rd Video
    Need 2 MORE

--- RubysAdventureRodriguezP7/Assets/Scripts/EnemyController.cs
+++ RubysAdventureRodriguezP7/Assets/Scripts/EnemyController.cs
@@ -8,7 +8,10 @@ public class EnemyController : MonoBehaviour
     public float speed = 3.0f;
     public bool veritcal;
     public float changeTime = 3.0f;
-    
+
+    public ParticleSystem smokeEffect;
+
+
     Rigidbody2D rigidbody2d;
 
     bool broken = true;
@@ -81,5 +84,6 @@ public class EnemyController : MonoBehaviour
         broken = false;
         rigidbody2d.simulated = false;
         animator.SetTrigger("Fixed");
+        smokeEffect.Stop();
     }
 }
